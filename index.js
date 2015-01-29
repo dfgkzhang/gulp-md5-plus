cf994aa9d3762d1077b0a7d5d15d10cae1561152
dfgkzhang <gary@digitalfoundry.com>
2015-01-28 16:01:16 -0800

    Fixed issue with Regex replacement

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports = function (size, ifile) {
         ifile && glob(ifile,function(err, files){
             if(err) return console.log(err);
             files.forEach(function(ilist){
-                var result = fs.readFileSync(ilist,'utf8').replace(new RegExp(sub_namepath + filename), sub_namepath + md5_filename);
+                var result = fs.readFileSync(ilist,'utf8').replace(new RegExp(sub_namepath + filename, 'g'), sub_namepath + md5_filename);
                 fs.writeFileSync(ilist, result, 'utf8');
             })
         })
